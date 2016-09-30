0196c75600c6d6b0a3363a65f9bd6e92d74a90be
gdsglgf <wzguofulgz@126.com>
2016-09-30 21:25:45 +0800

    add travis ci build status

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # json-formater
+
+[![Build Status](https://travis-ci.org/gdsglgf/json-formater.svg?branch=master)](https://travis-ci.org/gdsglgf/json-formater)
--- web/src/test/java/com/cims/mapper/UserMapperTest.java
+++ web/src/test/java/com/cims/mapper/UserMapperTest.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 @Rollback
-@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/dispatcher-servlet.xml"})
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/dispatcher-servlet.xml"})
 public class UserMapperTest {
     @Test
     public void testGetByUid() {
